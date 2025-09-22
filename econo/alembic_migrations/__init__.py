1353ed01b38c7017d34b528a46bd39bda2d3f041
JuanRojasB <199319087+JuanRojasB@users.noreply.github.com>
2025-09-22 16:58:41 -0500

    feat: Login y register exitoso

--- econo/alembic_migrations/__init__.py
+++
@@ -1,3 +0,0 @@
-#Establecer los componentes a importar a otros componentes
-from backend.app.db.session import engine, MARIADB_URL
-from .base import Base
--- econo/backend/app/models/rol.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from backend.app.db.session import Base
-
-class Rol(Base):
-    __tablename__ = "rol"
-
-    id_rol = Column(Integer, primary_key=True, autoincrement=True)
-    nombre_rol = Column(String(50), nullable=False)
